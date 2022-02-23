e64583ad0d99716517e6dabeed4bf07a82917331
Carlos Sosa <carlosneftaly@hotmail.com>
2022-02-23 12:07:23 -0600

    Clase 6 terminada

--- services/customer.service.js
+++ services/customer.service.js
@@ -1,13 +1,20 @@
 const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
 
 const { models } = require('./../libs/sequelize');
 class CustomerService {
   constructor() {}
 
   async create(data) {
+    const hash = await bcrypt.hash(data.user.password, 10);
+    data.user.password = hash;
+
     const newCustomer = await models.Customer.create(data, {
       include: ['user']
     });
+    console.log(newCustomer);
+
+    delete newCustomer.dataValues.user.dataValues.password;
     return newCustomer;
   }
 
